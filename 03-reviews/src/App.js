f9b8c94d490ae72774317fc12eb1ef1c30cef543
christy313 <christycreativity@gmail.com>
2022-09-10 20:39:51 -0400

    Finish project03

--- 03-reviews/src/App.js
+++ 03-reviews/src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import Review from "./Review";
 
 const App = () => {
-  return <main></main>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 };
 
 export default App;
