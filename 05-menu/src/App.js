6fcd32ea8f96aea9580364e2ec35b4d4c0450c78
christy313 <christycreativity@gmail.com>
2022-09-11 15:50:55 -0400

    Add Menu & Categories component

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -7,16 +7,7 @@ const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
+  const [categories, setCategories] = useState([]);
 
   return (
     <main>
@@ -25,7 +16,7 @@ const App = () => {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories categories={categories} filterItems={filterItems} />
+        <Categories />
         <Menu items={menuItems} />
       </section>
     </main>
