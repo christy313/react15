ab41cd7e681329f360c900c28ff6a2f2e9cefe00
christy313 <christycreativity@gmail.com>
2022-09-11 21:16:23 -0400

    Set up starter files

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
-function App() {
-  return <h2>tabs project setup</h2>
-}
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-export default App
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  return <h2>tabs project setup</h2>;
+};
+
+export default App;
