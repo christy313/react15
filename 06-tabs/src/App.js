d620a6b248109e0751403aa989409ce5509f7f9b
christy313 <christycreativity@gmail.com>
2022-09-12 16:34:29 -0400

    Restart

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,73 +1,9 @@
-import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
-
-  const fetchJobs = async () => {
-    const res = await fetch(url);
-    const newJobs = await res.json();
-
-    setJobs(newJobs);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchJobs();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="section loading">
-        <h1>loading...</h1>
-      </div>
-    );
-  }
-
-  const { title, dates, duties, company } = jobs[value];
-
-  return (
-    <div>
-      <section className="section">
-        <div className="title">
-          <h1>experiences</h1>
-          <div className="underline"></div>
-        </div>
-        <div className="jobs-center">
-          <div className="btn-container">
-            {jobs.map((job, index) => {
-              return (
-                <button
-                  onClick={() => setValue(index)}
-                  key={job.id}
-                  className={`job-btn ${index === value && "active-btn"}`}
-                >
-                  {job.company}
-                </button>
-              );
-            })}
-          </div>
-          <article className="job-info">
-            <h3>{title}</h3>
-            <h4>{company}</h4>
-            <p className="job-date">{dates}</p>
-            {duties.map((duty, index) => {
-              return (
-                <div className="job-desc" key={index}>
-                  <FaAngleDoubleRight className="job-icon" />
-                  <p>{duty}</p>
-                </div>
-              );
-            })}
-          </article>
-        </div>
-      </section>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
