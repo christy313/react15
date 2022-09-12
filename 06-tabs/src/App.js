853344992a017c43c5260eed33c3eeff81dbb462
christy313 <christycreativity@gmail.com>
2022-09-12 15:56:57 -0400

    Plan the whole picture

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -3,6 +3,27 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 
 const url = "https://course-api.com/react-tabs-project";
 
-const App = () => {};
+// loading
+// whole section overview
+// api
+// job desc
+// buttons
+
+const App = () => {
+  return (
+    <div>
+      <section className="section">
+        <div className="title">
+          <h1>experiences</h1>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          {/* buttons */}
+          <article className="job-info"></article>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default App;
