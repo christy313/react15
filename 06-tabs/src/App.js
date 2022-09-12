4be6dcebbf2ec26b7a4c8eed3b4bee8d655e536c
christy313 <christycreativity@gmail.com>
2022-09-12 14:28:50 -0400

    Fetch API

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -4,6 +4,22 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <div></div>;
 };
 
