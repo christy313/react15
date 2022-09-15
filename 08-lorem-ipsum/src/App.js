53ee7973a06f3ead335d86a3f3a5694f1d0a8122
christy313 <christycreativity@gmail.com>
2022-09-14 21:47:12 -0400

    Finish project 08

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -7,7 +7,17 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
@@ -25,13 +35,9 @@ const App = () => {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-          quaerat.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, autem?
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
