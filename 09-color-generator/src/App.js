3dc96c9d11cfb3429f34836f37e25d1f50c3edb2
christy313 <christycreativity@gmail.com>
2022-09-15 17:31:09 -0400

    Finish project 09

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -4,12 +4,20 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 const App = () => {
-  const [color, setColor] = useState("#f15250");
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -21,6 +29,8 @@ const App = () => {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             submit
@@ -28,7 +38,16 @@ const App = () => {
         </form>
       </section>
       <section className="colors">
-        <h4>hi</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
