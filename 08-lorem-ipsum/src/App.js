b17e291664d047bf2944791bbb7106c9a0318cda
christy313 <christycreativity@gmail.com>
2022-09-14 20:44:28 -0400

    Set up starter files

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
-import data from './data';
-function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
-}
+import React, { useState } from "react";
+import data from "./data";
+
+const App = () => {
+  return <h2>lorem ipsum project setup</h2>;
+};
 
 export default App;
