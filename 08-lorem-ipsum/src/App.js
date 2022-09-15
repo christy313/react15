662da3d4a26199178ee5ec1021cceef0be1ac0a2
christy313 <christycreativity@gmail.com>
2022-09-14 21:37:54 -0400

    Set up all structure

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -2,7 +2,39 @@ import React, { useState } from "react";
 import data from "./data";
 
 const App = () => {
-  return <section className="section-center"></section>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+          quaerat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, autem?
+        </p>
+      </article>
+    </section>
+  );
 };
 
 export default App;
