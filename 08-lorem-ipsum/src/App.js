8bc864a93ed94d5dc9d516a490973b0e7a55314b
christy313 <christycreativity@gmail.com>
2022-09-15 12:18:53 -0400

    Finish project 08

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -7,39 +7,26 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let amount = parseInt(count);
-    if (count <= 0) {
-      amount = 1;
-    }
-    if (count > data.length) {
-      amount = 8;
-    }
-    setText(data.slice(0, amount));
+    setText(data.slice(0, count));
   };
 
   return (
     <section className="section-center">
-      <h3>tired of boring lorem ipsum?</h3>
+      <h3>tired of boring lorem ipsum</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">paragraphs:</label>
+        <label htmlFor="">paragraphs:</label>
         <input
           type="number"
-          name="amount"
-          id="amount"
+          onChange={(e) => setCount(e.target.value)}
           value={count}
           min="1"
           max={data.length}
-          onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">
-          generate
-        </button>
+        <button className="btn">generate</button>
       </form>
-      <article className="lorem-text">
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>;
-        })}
-      </article>
+      {text.map((paragraph, index) => {
+        return <p key={index}>{paragraph}</p>;
+      })}
     </section>
   );
 };
