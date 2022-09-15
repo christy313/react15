fcf4f59ec932161095cfa4639706f0048ee0f166
christy313 <christycreativity@gmail.com>
2022-09-14 20:15:31 -0400

    Refactor title as Title component

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -3,6 +3,8 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 
+import Title from "./components/Title";
+
 const App = () => {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
@@ -28,11 +30,7 @@ const App = () => {
 
   return (
     <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>reviews
-        </h2>
-      </div>
+      <Title />
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, name, image, title, quote } = person;
