d8615d13e2444d41a8a267029299b15a384f70e2
christy313 <christycreativity@gmail.com>
2022-09-16 14:57:41 -0400

    Set up basic structure

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -9,7 +9,35 @@ const App = () => {
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  return <section className="section-center"></section>;
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log("hi");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handelSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 };
 
 export default App;
