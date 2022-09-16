f4863b8fc79ce18756bcffaca91bf08bdebb66f6
christy313 <christycreativity@gmail.com>
2022-09-15 20:50:54 -0400

    Finish project 09

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -5,19 +5,13 @@ import Values from "values.js";
 
 const App = () => {
   const [color, setColor] = useState("");
-  const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values("#f15025").all(10));
+  const [list, setList] = useState(new Values("#f12345").all(10));
 
   const handleSubmit = (e) => {
+    setList(new Values(color).all(10));
     e.preventDefault();
-    try {
-      let colors = new Values(color).all(10);
-      setList(colors);
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
   };
+
   return (
     <>
       <section className="container">
@@ -27,8 +21,7 @@ const App = () => {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
-            className={`${error ? "error" : null}`}
+            placeholder="#f123345"
           />
           <button className="btn" type="submit">
             submit
@@ -40,8 +33,8 @@ const App = () => {
           return (
             <SingleColor
               key={index}
-              {...color}
               index={index}
+              {...color}
               hexColor={color.hex}
             />
           );
--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
-const SingleColor = ({ rgb, weight, index, hexColor }) => {
+const SingleColor = ({ weight, rgb, hexColor, index }) => {
   const [alert, setAlert] = useState(false);
-  const bcg = rgb.join(",");
-  const hex = rgbToHex(...rgb);
+
   const hexValue = `#${hexColor}`;
+  const backgroundColor = rgb.join(", ");
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setAlert(false);
     }, 2000);
     return () => clearTimeout(timeout);
-  });
+  }, [alert]);
 
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}
+      style={{ backgroundColor: `rgb(${backgroundColor})` }}
       onClick={() => {
         setAlert(true);
         navigator.clipboard.writeText(hexValue);
@@ -25,7 +25,7 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
-      {alert && <p className="alert">copied to clipboard</p>}
+      {alert && <p>copied to clipboard</p>}
     </article>
   );
 };
