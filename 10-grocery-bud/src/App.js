3ad16118961ea702a1fad9ea0a67309ed5a41696
christy313 <christycreativity@gmail.com>
2022-09-19 21:31:25 -0400

    Set up states

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -3,6 +3,11 @@ import List from "./List";
 import Alert from "./Alert";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   return <section className="section-center"></section>;
 };
 
