6fdea3b8d5f3decf61c3b3d8411a788cd7d42d04
christy313 <christycreativity@gmail.com>
2022-09-19 20:20:36 -0400

    Set up states

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -3,7 +3,28 @@ import List from "./List";
 import Alert from "./Alert";
 
 const App = () => {
-  return <section className="section-center"></section>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hi");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 };
 
 export default App;
