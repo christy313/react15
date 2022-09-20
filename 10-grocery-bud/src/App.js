85e7ba6f95b01b4d8179c0b686243c955468809c
christy313 <christycreativity@gmail.com>
2022-09-19 20:30:44 -0400

    Add new item

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -11,16 +11,38 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hi");
+    if (!name) {
+      // alert
+    } else if (name && isEditing) {
+      // edit
+    } else {
+      // add items
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
       </form>
       <div className="grocery-container">
-        <List />
+        <List items={list} />
         <button className="clear-btn">clear items</button>
       </div>
     </section>
