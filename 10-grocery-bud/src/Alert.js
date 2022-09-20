ac6aee0ad0a02041a2eaee095cf640e36324491c
christy313 <christycreativity@gmail.com>
2022-09-19 20:14:06 -0400

    Basic set up

--- 10-grocery-bud/src/Alert.js
+++ 10-grocery-bud/src/Alert.js
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 
-const Alert = ({ type, msg, removeAlert, list }) => {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      removeAlert();
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, [removeAlert, list]);
-
-  return <p className={`alert alert-${type}`}>{msg}</p>;
+const Alert = () => {
+  return <p></p>;
 };
 
 export default Alert;
--- 10-grocery-bud/src/List.js
+++ 10-grocery-bud/src/List.js
@@ -1,35 +1,8 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-const List = ({ items, removeItem, editItem }) => {
-  return (
-    <div className="grocery-list">
-      {items.map((item) => {
-        const { id, title } = item;
-        return (
-          <article key={id} className="grocery-item">
-            <p className="title">{title}</p>
-            <div className="btn-container">
-              <button
-                type="button"
-                className="edit-btn"
-                onClick={() => editItem(id)}
-              >
-                <FaEdit />
-              </button>
-              <button
-                type="button"
-                className="delete-btn"
-                onClick={() => removeItem(id)}
-              >
-                <FaTrash />
-              </button>
-            </div>
-          </article>
-        );
-      })}
-    </div>
-  );
+const List = () => {
+  return <div className="grocery-list"></div>;
 };
 
 export default List;
